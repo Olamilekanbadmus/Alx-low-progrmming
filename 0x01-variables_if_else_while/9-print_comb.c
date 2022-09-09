1a24c4b0f67dfb4b384583c064f960e494a4e2d5
olamilekanbadmus <oluwaginggerayomi@gmail.com>
2022-09-09 13:39:20 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,20 +7,21 @@
 
 int main(void)
 {
-		int n = '0';
+	int n = '0';
 
-		while (n <= '9')
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
 		{
-			putchar(n);
-			if (n != '9')
-			{
-				putchar(',');
-				putchar(' ');
-			}			
-			n++;
-													
-		}
-		putchar('\n');
+			putchar(',');
+			putchar(' ');
 
-		return (0);
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
 }
+
