9de9a7e365a50753208cbef20a958df4b6cc5a52
olamilekanbadmus <oluwaginggerayomi@gmail.com>
2022-09-15 13:30:35 -0700

    1main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,16 +8,15 @@
 int main(void)
 {
 	int i;
-	/**
-	* variable decleration
-	*/
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	while (i < 10)
-	{
+/*
+ /* 	while (i < 10)
+ /*
+ /* 	{
 		putchar(i);
 	}
 
