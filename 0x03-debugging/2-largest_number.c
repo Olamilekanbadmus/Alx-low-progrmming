64932d5d31a74f71fbeb2acf87155ec22f948ce0
olamilekanbadmus <oluwaginggerayomi@gmail.com>
2022-09-16 03:14:02 -0700

    main

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,33 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
+	if (a >= b && b >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && a >= c)
+	{
+		largest = b;
+	}
+	else if (a >= c && c >= a)
+	{
+		largest = a;
+	}
+	else if (b >= c && c >= a)
+	{
+		largest = b;
+	}
+	else if (c >= a && a >= b)
+	{
+		largest = c;
+	}
+	else if (c >= b && b >= a)
+	{
+		largest = c;
+	}
+	else
+		largest = a;
+	return (largest);
 }
